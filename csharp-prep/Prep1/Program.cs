429b27e5da93346404149e2b14592874fa50f8a4
ezracrandall <ezracrandall2003@gmail.com>
2024-04-27 15:43:46 -0600

    prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,7 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your favorite color? ");
-        string color = Console.ReadLine();
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+       Console.WriteLine($"Your name is {last}, {first} {last}.");
+
     }
 }
