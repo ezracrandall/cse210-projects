6f387f62ba6cf4ef35c7047b22b51ddedbe6890f
ezracrandall <134823752+ezracrandall@users.noreply.github.com>
2024-05-01 17:21:42 -0600

    update

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -12,10 +12,10 @@ class Program
         do
         {
             Console.WriteLine("Menu:");
-            Console.WriteLine("1. Write a new entry");
-            Console.WriteLine("2. Display the journal");
-            Console.WriteLine("3. Save the journal to a file");
-            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Read");
+            Console.WriteLine("3. Save to file");
+            Console.WriteLine("4. Load from file ");
             Console.WriteLine("5. Exit");
             Console.Write("Enter your choice: ");
             choice = Console.ReadLine();
